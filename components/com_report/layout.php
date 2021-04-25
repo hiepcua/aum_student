c1ddfcd9c58078bdd6b0ad9a8aee5ea5ddbc1391
hiepcua <tranviethiepdz@gmail.com>
2021-04-26 05:24:52 +0700

    remove permission user when access to components

--- components/com_report/layout.php
+++ components/com_report/layout.php
@@ -1,8 +1,5 @@
 <?php 
 defined("ISHOME") or die("Can not acess this page, please come back!");
-$check_permission = $UserLogin->Permission('report');
-if($check_permission==false) die($GLOBALS['MSG_PERMIS']);
-
 $COMS="report";
 define('THIS_COM_PATH',COM_PATH.'com_'.$COMS.'/');
 $objUser=new CLS_USER();
